70df49951ba387f25b19124e8f4327e9a96db2ef
TheBrokenRail <connor24nolan@live.com>
2018-04-06 18:49:22 -0400

    Add Build Script

---
+++ Assets/Editor/Util.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+public class Util {
+	static public void Build() {
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = new[] {"Assets/Scene.unity"};
+		buildPlayerOptions.locationPathName = "Build/Win32/RogueRobots.exe";
+		buildPlayerOptions.target = BuildTarget.StandaloneWindows;
+		buildPlayerOptions.options = BuildOptions.None;
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+		buildPlayerOptions.locationPathName = "Build/Win64/RogueRobots.exe";
+		buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+		buildPlayerOptions.locationPathName = "Build/Linux/RogueRobots";
+		buildPlayerOptions.target = BuildTarget.StandaloneLinuxUniversal;
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+		buildPlayerOptions.locationPathName = "Build/OSX/RogueRobots";
+		buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+		BuildPipeline.BuildPlayer(buildPlayerOptions);
+	}
+}
