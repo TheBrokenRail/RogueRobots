d944605465fd1f48c4252e7f296224186ea07c69
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-07 16:56:35 -0400

    Update Install.bat

--- Scripts/Install.bat
+++ Scripts/Install.bat
@@ -10,10 +10,11 @@ call :install UnitySetup64 Windows64EditorInstaller
 call :install UnitySetup-Linux-Support-for-Editor-%UNITY_VERSION% TargetSupportInstaller
 call :install UnitySetup-WebGL-Support-for-Editor-%UNITY_VERSION% TargetSupportInstaller
 call :install UnitySetup-Windows-Support-for-Editor-%UNITY_VERSION% TargetSupportInstaller
+exit /b
 
 :install
 echo Downloading %~1.exe
 curl --retry 5 -O "https://netstorage.unity3d.com/unity/%UNITY_HASH%/%~2/%~1.exe"
-echo Installing $1.exe
+echo Installing %~1.exe
 %~1.exe /S
 exit /b
