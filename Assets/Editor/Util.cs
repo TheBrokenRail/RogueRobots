856a7dfd1004930f4a4bc2c1a6c7dc991bfaf679
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-06 23:15:07 -0400

    Update Util.cs

--- Assets/Editor/Util.cs
+++ Assets/Editor/Util.cs
@@ -9,13 +9,19 @@ public class Util {
 		buildPlayerOptions.locationPathName = "Build/Win32/RogueRobots.exe";
 		buildPlayerOptions.target = BuildTarget.StandaloneWindows;
 		buildPlayerOptions.options = BuildOptions.None;
+		Debug.Log("*** Started Windows 32bit Build! ***");
 		BuildPipeline.BuildPlayer(buildPlayerOptions);
+		Debug.Log("*** Finished Windows 32bit Build! ***");
 		buildPlayerOptions.locationPathName = "Build/Win64/RogueRobots.exe";
 		buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+		Debug.Log("*** Started Windows 64bit Build! ***");
 		BuildPipeline.BuildPlayer(buildPlayerOptions);
+		Debug.Log("*** Finished Windows 64bit Build! ***");
 		buildPlayerOptions.locationPathName = "Build/Linux/RogueRobots";
 		buildPlayerOptions.target = BuildTarget.StandaloneLinuxUniversal;
+		Debug.Log("*** Started Linux Build! ***");
 		BuildPipeline.BuildPlayer(buildPlayerOptions);
+		Debug.Log("*** Finished Linux Build! ***");
 		EditorApplication.Exit(0);
 	}
 }
