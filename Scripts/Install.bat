b1d7c5bf5f5ce5e25e842e5a6613afbcafc8024d
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2018-04-09 21:06:18 -0400

    Update Install.bat

--- Scripts/Install.bat
+++ Scripts/Install.bat
@@ -1,22 +1,19 @@
-@echo off
+#!/bin/sh
 
-set UNITY_HASH=fc1d3344e6ea
-set UNITY_VERSION=2017.3.1f1
+UNITY_HASH=fc1d3344e6ea
+UNITY_VERSION=2017.3.1f1
 
 mkdir UnityDownload
 cd UnityDownload
 
-call :install UnitySetup64 Windows64EditorInstaller
-call :install UnitySetup-Linux-Support-for-Editor-%UNITY_VERSION% TargetSupportInstaller
-call :install UnitySetup-WebGL-Support-for-Editor-%UNITY_VERSION% TargetSupportInstaller
-call :install UnitySetup-Mac-Support-for-Editor-%UNITY_VERSION% TargetSupportInstaller
+install () {
+  echo 'Downloading '"$1"'.pkg'
+  curl --retry 5 -O "https://netstorage.unity3d.com/unity/$UNITY_HASH/$2/$1.pkg"
+  echo 'Installing '"$1"'.pkg'
+  sudo installer -dumplog -package "$1.pkg" -target /
+}
 
-cd ..\
-exit /b
-
-:install
-echo Downloading %~1.exe
-curl --retry 5 -O "https://netstorage.unity3d.com/unity/%UNITY_HASH%/%~2/%~1.exe"
-echo Installing %~1.exe
-%~1.exe /S
-exit /b
+install Unity MacEditorInstaller
+install UnitySetup-Linux-Support-for-Editor-$UNITY_VERSION MacEditorTargetInstaller
+install UnitySetup-WebGL-Support-for-Editor-$UNITY_VERSION MacEditorTargetInstaller
+install UnitySetup-Windows-Support-for-Editor-$UNITY_VERSION MacEditorTargetInstaller
